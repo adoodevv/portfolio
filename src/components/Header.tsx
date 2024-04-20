852c5eabf398717412ca963e16c9930c2f7833f3
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-04-20 22:51:20 +0000

    modify ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,28 +3,28 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
    return (
-      <header className="text-gray-600 body-font">
-         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+      <header className="text-white bg-none fixed top-0 left-0 right-0">
+         <div className="md:container md:mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
             <nav className="flex lg:w-1/3 flex-wrap items-center text-sm md:ml-auto justify-start">
                <a href="#about" className="mr-5 hover:text-gray-900">Projects</a>
                <a href="#projects" className="mr-5 hover:text-gray-900">Achievements</a>
-               <a href="#contact" className="mr-5 hover:text-gray-900">Contact</a>
                <a href="#gallery" className="mr-5 hover:text-gray-900">Gallery</a>
             </nav>
-            <a className="flex lg:w-1/3 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+            <a className="flex lg:w-1/3 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0">
                <span className="ml-3 text-xl">adoodevv</span>
             </a>
             <div className="flex lg:w-1/3 justify-end">
-               <button>
-                  <a href="#contact" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-sm mt-4 md:mt-0">
-                  <FontAwesomeIcon icon={faDownload} className="text-bg-gray-100 mr-2" />
-                     CV
-                  </a>
+               <button className="text-white text-sm rounded-full mr-5 border p-2 hover:bg-blue-500 hover:border-none transition-colors duration-200">
+                  <FontAwesomeIcon icon={faDownload} className="mr-2" />
+                  CV
+               </button>
+               <button className="text-white text-sm rounded-full border p-2 hover:bg-white hover:text-black">
+                  Contact
                </button>
             </div>
          </div>
       </header>
-   )
-}
+   );
+};
 
-export default Header
+export default Header;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Header from './Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=" relative h-screen bg-cover bg-center" style={{backgroundImage: "url('src/assets/space.jpg')"}}>
+      <Header/>
+      <div className="h-full bg-black bg-opacity-50 flex items-center justify-start pl-20">
+        <div className="text-white">
+          <h1 className="text-6xl font-bold">ADOO Jonathan</h1>
+          <p className="text-2xl">A short description of me</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
