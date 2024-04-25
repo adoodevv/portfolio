9c9e54053f41eb62b9b5863d0ab0b931f48c23fc
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-04-25 19:32:29 +0000

    routing for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Home from "./components/Home"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Achievements from './components/Achievements';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div>
-      <Home/>
-    </div>
-  )
-}
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="*" element={<div>404: Not Found</div>} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+};
 
-export default App
+export default App;
