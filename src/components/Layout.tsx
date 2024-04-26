8e31d1f0703aefb3ee942887c922182c1df57b61
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-04-26 03:50:07 +0000

    bug fix for home hashlink

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,10 +8,10 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
    return (
       <div className="min-h-screen flex flex-col">
-         <nav className="bg-gray-800 text-white px-8 py-4 flex justify-between items-center">
+         <nav className="bg-gray-800 text-white px-8 py-4 flex justify-between items-center fixed w-full top-0">
             <ul className="text-sm flex space-x-4">
                <li>
-                  <Link smooth to="/" className="hover:text-gray-400">Home</Link>
+                  <Link smooth to="/#" className="hover:text-gray-400">Home</Link>
                </li>
                <li>
                   <Link smooth to="/#about" className="hover:text-gray-400">About</Link>
@@ -20,7 +20,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                   <Link smooth to="/#projects" className="hover:text-gray-400">Projects</Link>
                </li>
             </ul>
-            <Link to="/" className="text-xl font-bold">adoodevv</Link>
+            <Link to="/#" className="text-xl font-bold">adoodevv</Link>
             <ul className="text-sm flex space-x-4">
                <li>
                   <Link smooth to="/#contact" className="hover:text-gray-400">Contact</Link>
