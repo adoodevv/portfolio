0a4ee6efd4e3842001e4c17020af4b31a12b888f
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-05-01 06:34:49 +0000

    icrease button sizes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -26,17 +26,17 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="min-h-screen flex flex-col">
          <nav className={`px-8 py-4 flex justify-between items-center fixed w-full top-0 ${isScrolled ? 'bg-white bg-opacity-60 text-black' : 'bg-tranparent text-white'}`}>
             <ul className="text-sm flex space-x-4">
-               <button className="px-3 py-1 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 hover:text-white">
+               <button className="px-4 py-2 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 hover:text-white">
                   <li>
                      <Link smooth to="/#">Home</Link>
                   </li>
                </button>
-               <button className="px-3 py-1 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 hover:text-white">
+               <button className="px-4 py-2 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 hover:text-white">
                   <li>
                      <Link smooth to="/#about">About</Link>
                   </li>
                </button>
-               <button className="px-3 py-1 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 hover:text-white">
+               <button className="px-4 py-2 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 hover:text-white">
                   <li>
                      <Link smooth to="/#projects">Projects</Link>
                   </li>
@@ -45,17 +45,17 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             <Link to="/#" className="text-xl font-bold">adoodevv</Link>
             <ul className="text-sm flex space-x-4">
                <li>
-                  <button className={`px-3 py-1 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 ${isScrolled ? 'border border-black hover:text-white' : 'border border-white'}`}>
+                  <button className={`px-4 py-2 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 ${isScrolled ? 'border border-black hover:text-white' : 'border border-white'}`}>
                      <Link smooth to="/#contact">Contact</Link>
                   </button>
                </li>
                <li>
-                  <button className={`px-3 py-1 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 ${isScrolled ? 'border border-black hover:text-white' : 'border border-white'}`}>
+                  <button className={`px-4 py-2 rounded-full bg-transparent hover:border-sky-700 hover:bg-sky-700 ${isScrolled ? 'border border-black hover:text-white' : 'border border-white'}`}>
                      <Link smooth to="/#achievements">Achievements</Link>
                   </button>
                </li>
                <li>
-                  <button className="text-sky-700 px-3 py-1 rounded-full border border-white bg-white hover:scale-110">
+                  <button className="text-sky-700 px-4 py-2 rounded-full border border-white bg-white hover:scale-110">
                      <a href="/path/to/my/cv.pdf" download className="flex items-center space-x-1">
                         <FontAwesomeIcon icon={faDownload} />
                         <span>CV</span>
