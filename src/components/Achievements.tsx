798efd1a6149ce9f9be040994cf9b4e358b3f167
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-07-16 03:19:49 +0000

    minor changes

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -5,13 +5,17 @@ const Achievements: React.FC = () => {
             <div className="absolute left-0 top-0 bottom-0 w-1 h-8 bg-sky-700"></div>
             <h2 className="text-2xl font-medium text-white mb-4">Achievements</h2>
          </div>
-         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-white shadow rounded-lg p-4">
+         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="bg-white shadow rounded-3xl p-4">
                <h3 className="text-xl font-medium text-gray-900 mb-2">Achievement 1</h3>
                <p className="text-gray-700">A brief description of the achievement</p>
             </div>
-            <div className="bg-white shadow rounded-lg p-4">
+            <div className="bg-white shadow rounded-3xl p-4">
                <h3 className="text-xl font-medium text-gray-900 mb-2">Achievement 2</h3>
+               <p className="text-gray-700">A brief description of the achievement</p>
+            </div>
+            <div className="bg-white shadow rounded-3xl p-4">
+               <h3 className="text-xl font-medium text-gray-900 mb-2">Achievement 3</h3>
                <p className="text-gray-700">A brief description of the achievement</p>
             </div>
          </div>
