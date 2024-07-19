d5718bb95794b4bbd7f3d8058084a02ff5f92c09
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-07-19 19:05:55 +0000

    new components for skills section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,26 @@
+const Skills: React.FC = () => {
+   return (
+      <div className="relative py-16 bg-sky-700 bg-opacity-10">
+         <ul className="flex items-center justify-between flex-row px-16">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" className="w-16 h-16 hover:animate-spin-slow" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" className="w-16 h-16 hover:animate-spin-slow" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" className="w-16 h-16 hover:animate-spin-slow" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ros/ros-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gazebo/gazebo-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg" className="w-16 h-16 hover:animate-spin-slow" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/opencv/opencv-original.svg" className="w-16 h-16 hover:animate-spin-slow" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/raspberrypi/raspberrypi-original.svg" className="w-16 h-16 hover:animate-wiggle" />
+         </ul>
+      </div>
+
+   );
+};
+
+export default Skills;
